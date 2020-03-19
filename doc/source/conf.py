95b8415d39a560f4edcd94ac1726d42b66e8d4fc
Tobias Uelwer <tobias.uelwer@uni-duesseldorf.de>
2020-03-19 15:43:31 +0100

    Update version

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,7 +25,7 @@ copyright = "2019, Thomas Germer, Tobias Uelwer, Stefan Conrad, Stefan Harmeling
 author = "Thomas Germer, Tobias Uelwer, Stefan Conrad, Stefan Harmeling"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+release = "1.0.6"
 
 
 # -- General configuration ---------------------------------------------------
