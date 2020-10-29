838171bdf13ddd474c1e81b7a4f1427bf6da6703
99991 <99991@users.noreply.github.com>
2020-10-29 10:14:19 +0100

    Bump version number in docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,7 +25,7 @@ copyright = "2020, Thomas Germer, Tobias Uelwer, Stefan Conrad, Stefan Harmeling
 author = "Thomas Germer, Tobias Uelwer, Stefan Conrad, Stefan Harmeling"
 
 # The full version, including alpha/beta/rc tags
-release = "1.1.0"
+release = "1.1.1"
 
 
 # -- General configuration ---------------------------------------------------
