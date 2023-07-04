d803f9ad242d9704017c0fc78cbc905c7c343337
Thomas Germer <99991@users.noreply.github.com>
2023-07-04 10:45:56 +0200

    Improve formatting of documentation

--- pymatting/preconditioner/jacobi.py
+++ pymatting/preconditioner/jacobi.py
@@ -3,12 +3,12 @@ def jacobi(A):
     Compute the Jacobi preconditioner function for the matrix A.
 
     Parameters
-    -----------
+    ----------
     A: np.array
         Input matrix to compute the Jacobi preconditioner for.
 
     Returns
-    ---------
+    -------
     precondition_matvec: function
         Function which applies the Jacobi preconditioner to a vector
 
