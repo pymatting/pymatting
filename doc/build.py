632f28834a06a7ba4ffeb60e0d02ba6a5efee824
99991 <99991@users.noreply.github.com>
2023-10-06 09:07:31 +0200

    Fix Path removing double forward slash

--- doc/build.py
+++ doc/build.py
@@ -124,7 +124,8 @@ def generate_html(node, references, html=None):
         node["fullname"] = fullname
 
         source_url = "".join([
-            str(Path("https://github.com/pymatting/pymatting/blob/master/pymatting") / Path(node["filename"]).relative_to(src_dir)),
+            "https://",
+            str(Path("github.com/pymatting/pymatting/blob/master/pymatting") / Path(node["filename"]).relative_to(src_dir)),
             "#L",
             str(node["lineno"]),
             "-L",
