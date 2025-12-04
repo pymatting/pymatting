c78244332f278c615003ccad3913f9f976ac3526
Amit Charan <amitcharan911@gmail.com>
2025-12-04 16:23:43 +0530

    added try-except in imports

--- pymatting/foreground/__init__.py
+++ pymatting/foreground/__init__.py
@@ -3,5 +3,8 @@ from pymatting.foreground.estimate_foreground_ml import estimate_foreground_ml
 from pymatting.foreground.estimate_foreground_ml import (
     estimate_foreground_ml as estimate_foreground,
 )
-from pymatting.foreground.estimate_foreground_ml_cupy import estimate_foreground_ml_cupy
-from pymatting.foreground.estimate_foreground_ml_pyopencl import estimate_foreground_ml_pyopencl
+try:
+    from pymatting.foreground.estimate_foreground_ml_cupy import estimate_foreground_ml_cupy
+    from pymatting.foreground.estimate_foreground_ml_pyopencl import estimate_foreground_ml_pyopencl
+except ModuleNotFoundError:
+    pass
